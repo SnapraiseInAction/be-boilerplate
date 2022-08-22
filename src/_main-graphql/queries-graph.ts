54dfbf91440dfab533030238eaf4284b79c5223a
Reginald Osano <regie30developer@gmail.com>
2022-08-22 17:03:25 +0800

    Init for be-main-apollo-gateway

--- src/_main-graphql/queries-graph.ts
+++
@@ -1,6 +0,0 @@
-export const AllQueriesGQL = `
-type Query {
-    users: [User]
-    user(id: Int!): User
-  }
-`;
--- src/authentication/graphql/auth-gql.ts
+++
@@ -1,6 +0,0 @@
-export const AuthType = `
-type Auth {
-    user: User
-    token: String
-    cookie: String
-}`;
